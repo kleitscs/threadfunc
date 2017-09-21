788ee1e4df95d92352f7394f95f5aa27a3f22463
kleitscs <kleitscs@gmail.com>
2017-09-21 10:27:52 -0500

    Removed useless comment

--- pitch.py
+++ pitch.py
@@ -16,9 +16,6 @@ def name(part):
     cursor.close()
 
 
-#This line added
-
-
 def unique(part):
     cursor.execute('''SELECT b.PitchID
                         FROM [LocknutData].[dbo].[PartNumber] a
